e964a7a6935690215c874c14c5dedf2060848d09
0xrezza <rezaghoreyshi.contact@gmail.com>
2025-09-15 13:16:25 +0330

    create enter raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,5 +1,26 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
 /**
  * @title Raffle
@@ -7,15 +28,27 @@ pragma solidity ^0.8.19;
  * @notice This contract is for creating a sample raffle
  * @dev Implements Chainlink VRFv2.5
  */
-
 contract Raffle {
+    error Raffle__SendMoreToInterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        // require(msg.value >= i_entranceFee, SendMoreToInterRaffle());
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToInterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {}
 
